36977269fce176999a090f1fea5ecf858f1e9d85
yr0418 <1933352594@qq.com>
2020-05-04 21:39:16 +0800

    添加ask_url 函数，获取网页源码

--- main/main.py
+++ main/main.py
@@ -14,7 +14,7 @@ import pymysql
 def main():
     # 基础Url，起始页
     base_url = "https://movie.douban.com/top250?start="
-
+    ask_url(base_url)
     # 1.爬取网页
     datalist = get_data(base_url)
 
@@ -22,8 +22,30 @@ def main():
 # 2.获取网页数据，边爬边解析
 def get_data(base_url):
     datalist = []
-    pass
+    for i in range(0, 10):
+        url = base_url + str(i*25)
+        html = ask_url(url)  # 获取一页的HTML网页源码
     return datalist
+
+
+# 得到一个指定url的网页内容，返回字符串html，封装网页源码
+def ask_url(url):
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36"
+    }
+    # 模拟请求
+    req = urllib.request.Request(url=url, headers=headers, method="GET")
+    respond = urllib.request.urlopen(req)
+    html = ""
+    try:
+        html = respond.read().decode("utf-8")
+        # print(html)
+    except urllib.error.URLError as e:
+        if hasattr(e, "code"):
+            print("！！！ask_url，error.code: ", e.code)
+        if hasattr(e, "reason"):
+            print("！！！ask_url，error.reason: ", e.reason)
+    return html
 
 
 # 保存数据，将数据保存至MySQL数据库
